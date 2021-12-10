3dce14373d19dc6c63c62c71a575d6ea75811135
charliegallop <c.gallop@protonmail.com>
2021-12-10 13:30:30 +0000

    First commit of all files

---
+++ netcdf4.py
@@ -0,0 +1,30 @@
+# %%
+
+import netCDF4 as nc
+import numpy as np
+
+fn = "CEH_GEAR_monthly_GB_2019.nc"
+
+ds = nc.Dataset(fn)
+rainfall = ds['rainfall_amount'][:]
+y = ds['y'][:]
+x = ds['x'][:]
+lat
+# %%
+rainfall.shape
+# %%
+for i in range(12):
+    print(rainfall[i, 501, 351])
+    print(y[501])
+    print(x[351])
+
+# %%
+
+print(ds.variables.keys()) # get all variable names
+
+# %%
+sst = ds.variables['rainfall_amount'] # sst variable
+time = ds.variables['time']
+print(time)
+
+# %%
