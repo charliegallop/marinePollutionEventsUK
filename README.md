5847c99929fad9bb85f433c20d0973014b18dc9c
charliegallop <c.gallop@protonmail.com>
2021-12-11 15:33:37 +0000

    Further cleaning of the water quality data and added a todo list

---
+++ README.md
@@ -0,0 +1,4 @@
+## TODO:
+
+- [ ] Figure out thresholds for determining when there is a pollution event. This could be looking at bacteria or sewage determinands etc
+- [ ] May need to pivot the data. each determinand would need its own column. No idea how to do this yet
--- code/rawCode/dataCleaningWaterQual.py
+++ code/rawCode/dataCleaningWaterQual.py
@@ -3,23 +3,38 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt 
 
+# %%
 n = 10000
 years = ['2009']
 
 rawData = pd.read_csv(f"../../data/rawData/{years[0]}_waterQuality.csv")
 # %%
 
-# drop columns that are not needed
-df1 = rawData.drop(['sample.samplingPoint', 'codedResultInterpretation.interpretation', 'sample.isComplianceSample'], axis = 1)
-
-# remove the unecessary strings from the id column
-df1['@id'] = rawData['@id'].astype(str).str[-15:]
+# Selecting only the samples from sea water
+dfSea = rawData[rawData['sample.sampledMaterialType.label'] == 'SEA WATER']
+# %%
+# Remove time from dataTime column
+dfSea['sample.sampleDateTime'] = rawData['sample.sampleDateTime'].astype(str).str[:10]
 
 # %%
+#seperate dateTime column into day, month, year columns
+dfSea[['year', 'month', 'day']] = dfSea['sample.sampleDateTime'].str.split('-', expand = True)
 
-# Selecting only the samples from sea water
+# %%
+# drop columns that are not needed
+dfSea = rawData.drop(['sample.samplingPoint', 'codedResultInterpretation.interpretation', 'sample.isComplianceSample', 'sample.sampleDateTime'], axis = 1)
 
-dfSea = df1[df1['sample.sampledMaterialType.label'] == 'SEA WATER']
+# %%
+# remove the unecessary strings from the id column
+dfSea['@id'] = rawData['@id'].astype(str).str[-15:]
+
+
+# %%
+dfEcoli = dfSea[dfSea['determinand.label'] == "E.coli C-MF"]
+dfEcoli = dfEcoli[dfEcoli['result'] > 500]
+plt.hist(dfEcoli['result'], bins = [100, 200, 300, 400, 500, 600, 700])
+
 
 # %%
