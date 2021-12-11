ebe5d177474a668c688c321851f7f11b85ad58d3
charliegallop <c.gallop@protonmail.com>
2021-12-11 17:33:15 +0000

    Pivotted the data and have it in a format where I can start using it. Updated todo list

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## TODO:
 
 - [ ] Figure out thresholds for determining when there is a pollution event. This could be looking at bacteria or sewage determinands etc
-- [ ] May need to pivot the data. each determinand would need its own column. No idea how to do this yet
+- [X] May need to pivot the data. each determinand would need its own column. No idea how to do this yet
+- [ ] Turn the processing of cleaning and pivotting the datasets into a function so I can then do it for all years
--- code/rawCode/dataCleaningWaterQual.py
+++ code/rawCode/dataCleaningWaterQual.py
@@ -5,30 +5,60 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 
+
 # %%
 n = 10000
 years = ['2009']
 
 rawData = pd.read_csv(f"../../data/rawData/{years[0]}_waterQuality.csv")
+
+
 # %%
 
 # Selecting only the samples from sea water
 dfSea = rawData[rawData['sample.sampledMaterialType.label'] == 'SEA WATER']
-# %%
-# Remove time from dataTime column
-dfSea['sample.sampleDateTime'] = rawData['sample.sampleDateTime'].astype(str).str[:10]
+
 
 # %%
-#seperate dateTime column into day, month, year columns
-dfSea[['year', 'month', 'day']] = dfSea['sample.sampleDateTime'].str.split('-', expand = True)
+# Remove time from dataTime column
+dfSea['sample.sampleDateTime'] = dfSea['sample.sampleDateTime'].astype(str).str[:10]
+
+# # %%
+# #seperate dateTime column into day, month, year columns
+# dfSea[['year', 'month', 'day']] = dfSea['sample.sampleDateTime'].str.split('-', expand = True)
 
 # %%
 # drop columns that are not needed
-dfSea = rawData.drop(['sample.samplingPoint', 'codedResultInterpretation.interpretation', 'sample.isComplianceSample', 'sample.sampleDateTime'], axis = 1)
+dfSea = dfSea.drop(['sample.samplingPoint', 'codedResultInterpretation.interpretation', 'sample.isComplianceSample'], axis = 1)
 
 # %%
 # remove the unecessary strings from the id column
-dfSea['@id'] = rawData['@id'].astype(str).str[-15:]
+dfSea['@id'] = dfSea['@id'].astype(str).str[-15:]
+
+# %%
+# dfSea Pivotted
+dfSeaP = pd.pivot_table(dfSea, values = 'result', index = ['@id'], columns = 'determinand.label')
+dfSeaP = dfSeaP.dropna(axis=1, how='all')
+
+# %%
+dfSeaPMerged = dfSeaP.merge(dfSea[['@id', 'sample.samplingPoint.notation', 'sample.samplingPoint.label',
+       'sample.samplingPoint.easting', 'sample.samplingPoint.northing', 'sample.sampleDateTime']], how = 'left', on = '@id')
+dfSeaPMerged.head()
+
+# Group together so that each recording for each data and location is grouped together
+dfSeaPMerged = dfSeaPMerged.groupby(['sample.sampleDateTime', 'sample.samplingPoint.label', 'sample.samplingPoint.easting', 'sample.samplingPoint.northing']).sum().reset_index()
+
+# %%
+dfEcoli = dfSea[dfSea['determinand.label'] == "E.coli C-MF"]
+
+
+
+
+
+#%%
+dfEcoli = dfEcoli.drop(['sample.samplingPoint.notation', 'sample.samplingPoint.label', 'determinand.definition', 'determinand.notation', 'resultQualifier.notation', 'sample.purpose.label'], axis = 1)
+dfEcoli.pivot_table(index = ['@id', 'year', 'month','day'],  columns = 'determinand.label', values = ['result', 'determinand.unit.label', 'sample.samplingPoint.easting', 'sample.samplingPoint.northing'])
+
 
 
 # %%
